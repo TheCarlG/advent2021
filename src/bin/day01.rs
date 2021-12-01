91fd0df7ee7853954080af705d7da7f4f3c8d3e4
TheCarlG <me@carlg.tech>
2021-12-01 23:03:23 +0100

    day01: Fixup #5

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -4,23 +4,16 @@ fn main() {
     common::time_func(|| {
         let lines = common::read_input("input/day01.txt");
 
-        let mut prev = u32::MAX;
-        let mut j = 0;
-        let mut i = if lines[0] < lines[1] { 1 } else { 0 };
+        let p1 = lines.windows(2).filter(|x| x[0] < x[1]).count();
+        let p2 = lines
+            .windows(3)
+            .fold((u32::MAX, 0 as u32), |(prev, i), x| {
+                let sum = x[0] + x[1] + x[2];
+                (sum, i + if prev < sum { 1 } else { 0 })
+            })
+            .1;
 
-        lines.windows(3).for_each(|x| {
-            if x[1] < x[2] {
-                i += 1;
-            }
-
-            let val = x.into_iter().sum();
-            if prev < val {
-                j += 1;
-            }
-            prev = val;
-        });
-
-        println!("Part 1: {}", i);
-        println!("Part 2: {}", j);
+        println!("Part 1: {}", p1);
+        println!("Part 2: {}", p2);
     });
 }
