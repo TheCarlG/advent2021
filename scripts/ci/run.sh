163db1de8de1ba4d425593f2de508cd8905330fc
TheCarlG <me@carlg.tech>
2021-12-02 14:30:50 +0100

    Fixed workflow

--- scripts/ci/run.sh
+++ scripts/ci/run.sh
@@ -2,7 +2,6 @@
 i=0
 for e in $(find target/release/ -maxdepth 1 -executable -type f -name 'day*' | sort); do
   echo "::set-output name=name$i::**$(echo $e | cut -d/ -f3)**"
-  echo "::set-output name=result$i::$($e | tr '\n' "|")"
-  echo "::set-output name=separator$i::***"
+  echo "::set-output name=result$i::$($e | tr '\n' "\n")"
   ((i++))
 done
