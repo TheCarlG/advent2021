49e585382843343c71aa367f50a9abf143f7ef84
TheCarlG <me@carlg.tech>
2021-12-02 15:04:34 +0100

    Cleanup

--- scripts/ci/run.sh
+++ scripts/ci/run.sh
@@ -6,7 +6,7 @@ for e in $(find target/release/ -maxdepth 1 -executable -type f -name 'day*' | s
         echo "::set-output name=$n-result-$i::\`\`\`"
         $e | while IFS= read -r l; do 
                 ((i++))
-                echo "::set-output name=$n-result-$i::  $(echo $l | tr '\n' ' ')"
+                echo "::set-output name=$n-result-$i::$(echo $l | tr '\n' ' ')"
         done
         echo "::set-output name=$n-result-4::\`\`\`"
 done
--- src/main.rs
+++
@@ -1,5 +0,0 @@
-mod lib;
-
-fn main() {
-    println!("Hello, Advent of Code 2021!");
-}
