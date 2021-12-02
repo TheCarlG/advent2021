5de5fd2b97b70d9ed40d8f9b7b79070ed344ff89
TheCarlG <me@carlg.tech>
2021-12-02 14:52:09 +0100

    Fixed workflow

--- scripts/ci/run.sh
+++ scripts/ci/run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 for e in $(find target/release/ -maxdepth 1 -executable -type f -name 'day*' | sort); do
         n=$(echo $e | cut -d/ -f3)
-        i=0
         echo "::set-output name=$n-name::**$n**"
+        i=0
+        echo "::set-output name=$n-result-$i::\`\`\`"
         $e | while IFS= read -r l; do 
-                echo "::set-output name=$n-result-$i::  $(echo $l | tr '\n' ' ')"
                 ((i++))
+                echo "::set-output name=$n-result-$i::  $(echo $l | tr '\n' ' ')"
         done
-        ((i++))
-        echo "::set-output name=$n-result-$i::-"
+        echo "::set-output name=$n-result-4::\`\`\`"
 done
