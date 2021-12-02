fcdb824e589a3adbd2b88354f154b63627dee605
TheCarlG <me@carlg.tech>
2021-12-02 07:40:39 +0100

    day01: Fixup

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -1,10 +1,10 @@
 use advent2021::common;
 
-fn depth_measurement_count(l: &Vec<u32>) -> usize {
+fn part1(l: &Vec<u32>) -> usize {
     l.windows(2).filter(|x| x[0] < x[1]).count()
 }
 
-fn depth_measurement_slide_count(l: &Vec<u32>) -> usize {
+fn part2(l: &Vec<u32>) -> usize {
     l.windows(3)
         .fold((u32::MAX, 0 as usize), |(prev, i), x| {
             let sum = x[0] + x[1] + x[2];
@@ -17,11 +17,8 @@ fn main() {
     common::time_func(|| {
         let lines = common::read_input::<u32>("input/day01.txt");
 
-        let p1 = depth_measurement_count(&lines);
-        let p2 = depth_measurement_slide_count(&lines);
-
-        println!("Part 1: {}", p1);
-        println!("Part 2: {}", p2);
+        println!("Part 1: {}", part1(&lines));
+        println!("Part 2: {}", part2(&lines));
     });
 }
 
@@ -30,16 +27,16 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_depth_measurement_count() {
+    fn test_part1() {
         let test_data: Vec<u32> = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
 
-        assert_eq!(depth_measurement_count(&test_data), 7);
+        assert_eq!(part1(&test_data), 7);
     }
 
     #[test]
-    fn test_depth_measurement_slide_count() {
+    fn test_part2() {
         let test_data: Vec<u32> = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
 
-        assert_eq!(depth_measurement_slide_count(&test_data), 5);
+        assert_eq!(part2(&test_data), 5);
     }
 }
