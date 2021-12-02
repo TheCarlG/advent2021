a6d7d8ab98a9e8d7ba29f1aa19d3e1339e678405
TheCarlG <me@carlg.tech>
2021-12-02 12:40:06 +0100

    Fixed workflow

---
+++ scripts/ci/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+i=0
+for e in $(find target/release/ -maxdepth 1 -executable -type f -name 'day*' | sort); do
+  echo "::set-output name=name$i::$(echo $e | cut -d/ -f3)\n"
+  echo "::set-output name=result$i::$($e | tr '\n' "|")\n"
+  ((i++))
+done
