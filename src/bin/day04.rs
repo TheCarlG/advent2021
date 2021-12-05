e945f67e16530df0a783e2eb2467590d3bd2b452
TheCarlG <me@carlg.tech>
2021-12-05 02:22:03 +0100

    day04: part2 initial commit

--- src/bin/day04.rs
+++ src/bin/day04.rs
@@ -6,7 +6,7 @@ const BOARD_SIZE: usize = SIZE.pow(2);
 const H_MATCH: u32 = 0x1F;
 const V_MATCH: u32 = 0x108421;
 
-fn parse(mut lines: Vec<String>) -> (Vec<i32>, Vec<[i32; 25]>) {
+fn parse(mut lines: Vec<String>) -> (Vec<i32>, Vec<([i32; 25], u32)>) {
     let drawn = lines
         .remove(0)
         .split(',')
@@ -15,11 +15,11 @@ fn parse(mut lines: Vec<String>) -> (Vec<i32>, Vec<[i32; 25]>) {
         .collect::<Vec<i32>>();
 
     let (boards, _) = lines.iter().skip(1).fold(
-        (vec![[0_i32; BOARD_SIZE]], 0_usize),
+        (vec![([0_i32; BOARD_SIZE], 0_u32)], 0_usize),
         |(mut boards, mut y), row| {
             if row.is_empty() {
                 y = 0;
-                boards.push([0_i32; BOARD_SIZE]);
+                boards.push(([0_i32; BOARD_SIZE], 0_u32));
             } else {
                 let i = boards.len() - 1;
                 row.split(' ')
@@ -27,7 +27,7 @@ fn parse(mut lines: Vec<String>) -> (Vec<i32>, Vec<[i32; 25]>) {
                     .map(|n| n.parse::<i32>().unwrap())
                     .enumerate()
                     .for_each(|(x, n)| {
-                        boards[i][(SIZE * y + x)] = n;
+                        boards[i].0[(SIZE * y + x)] = n;
                     });
                 y += 1;
             }
@@ -38,44 +38,45 @@ fn parse(mut lines: Vec<String>) -> (Vec<i32>, Vec<[i32; 25]>) {
     (drawn, boards)
 }
 
-fn part1(drawn: &[i32], boards: &[[i32; 25]]) -> i32 {
-    let mut marked = vec![0_u32];
-    marked.resize(boards.len(), 0);
-    let mut w_board = 0;
-    let mut w_num = 0;
-    'outer: for num in drawn.iter() {
-        for (i, board) in boards.iter().enumerate() {
-            if let Some(idx) = board.iter().position(|&v| v == *num) {
-                marked[i] |= 1 << (idx);
-            }
-            for j in 0..BOARD_SIZE {
-                let tmp = marked[i] >> j;
-                if j % SIZE == 0 && (tmp & H_MATCH) == H_MATCH {
-                    w_board = i;
-                    w_num = *num;
-                    break 'outer;
+fn play(drawn: &[i32], mut boards: Vec<([i32; 25], u32)>) -> (i32, i32) {
+    let winners = drawn.iter().fold(Vec::new(), |mut winners, num| {
+        boards = boards
+            .iter()
+            .map(|&(mut board)| {
+                if let Some(idx) = board.0.iter().position(|&v| v == *num) {
+                    board.1 |= 1 << idx;
                 }
-                if (V_MATCH & tmp) == V_MATCH {
-                    w_board = i;
-                    w_num = *num;
-                    break 'outer;
+                board
+            })
+            .filter(|board| {
+                for j in 0..BOARD_SIZE {
+                    let tmp = board.1 >> j;
+                    if (j % SIZE == 0 && (tmp & H_MATCH) == H_MATCH) || (V_MATCH & tmp) == V_MATCH {
+                        winners.push((num, (board.0, board.1)));
+                        return false;
+                    }
                 }
-            }
-        }
-    }
+                true
+            })
+            .collect::<Vec<([i32; 25], u32)>>();
+        winners
+    });
 
-    let p = marked[w_board];
-    let mut sum = 0;
+    let (first_n, first_b) = winners.first().unwrap();
+    let (last_n, last_b) = winners.last().unwrap();
+
+    let mut first_s = 0;
+    let mut last_s = 0;
     for i in 0..BOARD_SIZE {
-        if (p >> i) & 1 != 1 {
-            sum += boards[w_board][i];
+        if (first_b.1 >> i) & 1 != 1 {
+            first_s += first_b.0[i];
+        }
+        if (last_b.1 >> i) & 1 != 1 {
+            last_s += last_b.0[i];
         }
     }
-    sum * w_num
-}
 
-fn part2(_drawn: &[i32], _boards: &[[i32; 25]]) -> u32 {
-    0
+    (first_s * **first_n, last_s * **last_n)
 }
 
 fn main() {
@@ -83,8 +84,9 @@ fn main() {
         let lines = common::read_input::<String>("input/day04.data");
         let (drawn, boards) = parse(lines);
 
-        println!("Part01: {}", part1(&drawn, &boards));
-        println!("Part02: {}", part2(&drawn, &boards));
+        let (part1, part2) = play(&drawn, boards);
+        println!("Part01: {}", part1);
+        println!("Part02: {}", part2);
     });
 }
 
@@ -97,17 +99,17 @@ mod tests {
         let lines = common::read_input::<String>("input/day04.test");
         let (drawn, boards) = parse(lines);
 
-        assert_eq!(part1(&drawn, &boards), 4512);
+        assert_eq!(play(&drawn, boards).0, 4512);
 
         let lines = common::read_input::<String>("input/day04-2.test");
         let (drawn, boards) = parse(lines);
-        assert_eq!(part1(&drawn, &boards), 2772);
+        assert_eq!(play(&drawn, boards).0, 2772);
     }
 
-    // #[test]
-    // fn test_part2() {
-    //     let lines = common::read_input::<String>("input/day04.test");
-    //     let (drawn, boards) = parse(lines);
-    //     assert_eq!(part2(&drawn, &boards), 0);
-    // }
+    #[test]
+    fn test_part2() {
+        let lines = common::read_input::<String>("input/day04.test");
+        let (drawn, boards) = parse(lines);
+        assert_eq!(play(&drawn, boards).1, 1924);
+    }
 }
