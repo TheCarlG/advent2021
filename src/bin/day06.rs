c8a9d7b6118e7b9677f7baceb994a0e9819d6b08
TheCarlG <me@carlg.tech>
2021-12-06 22:38:39 +0100

    day06: Fixed

--- src/bin/day06.rs
+++ src/bin/day06.rs
@@ -1,76 +1,18 @@
-use std::fmt::Display;
-use std::str::FromStr;
-
 use advent2021::common;
 
-#[derive(Clone, Debug, Copy)]
-struct Fish {
-    timer: i32,
-    count: u64,
-}
-
-impl Fish {
-    fn default(cnt: u64) -> Self {
-        Self {
-            timer: 8,
-            count: cnt,
-        }
-    }
-    fn new(cnt: u64) -> Self {
-        Self {
-            timer: 6,
-            count: cnt,
-        }
+fn simulate(input: &[u128], days: i32) -> [u128; 9] {
+    let mut data = [0_u128; 9];
+    for i in input {
+        data[*i as usize] += 1;
     }
 
-    fn decrease(&mut self) {
-        self.timer -= 1;
-    }
-}
-
-impl Display for Fish {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "({},{})", self.timer, self.count)
-    }
-}
-
-impl FromStr for Fish {
-    type Err = String;
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        Ok(Fish {
-            timer: s.parse::<i32>().unwrap(),
-            count: 1,
-        })
-    }
-}
-
-fn simulate(mut data: Vec<Fish>, days: i32) -> Vec<Fish> {
     for _ in 1..=days {
-        let mut cnt: u64 = 0;
-        data = data
-            .iter()
-            .filter(|f| {
-                if f.timer == -1 {
-                    cnt += f.count;
-                    false
-                } else {
-                    true
-                }
-            })
-            .cloned()
-            .collect();
-
-        if cnt > 0 {
-            data.push(Fish::new(cnt));
+        let new = data[0];
+        for i in 0..=7 {
+            data[i] = data[i + 1];
         }
-        for i in 0..data.len() {
-            let mut f = data[i];
-            if f.timer == 0 {
-                data.push(Fish::default(f.count));
-            }
-            f.decrease();
-            data[i] = f;
-        }
+        data[6] += new;
+        data[8] = new;
     }
 
     data
@@ -78,19 +20,19 @@ fn simulate(mut data: Vec<Fish>, days: i32) -> Vec<Fish> {
 
 fn main() {
     common::time_func(|| {
-        let data: Vec<Fish> = common::read_input::<String>("input/day06.data")
+        let input: Vec<u128> = common::read_input::<String>("input/day06.data")
             .first()
             .unwrap()
             .split(',')
-            .map(|v| v.parse::<Fish>().unwrap())
+            .map(|v| v.parse::<u128>().unwrap())
             .collect();
 
-        let data = simulate(data, 80);
-        let mut cnt = data.iter().fold(0_u64, |cnt, f| cnt + (f.count as u64));
+        let data = simulate(&input, 80);
+        let cnt: u128 = data.iter().sum();
         println!("Part01: {}", cnt);
 
-        let data = simulate(data, 256 - 80);
-        cnt = data.iter().fold(0_u64, |cnt, f| cnt + (f.count as u64));
+        let data = simulate(&input, 256);
+        let cnt: u128 = data.iter().sum();
         println!("Part02: {}", cnt);
     });
 }
@@ -101,44 +43,43 @@ mod tests {
 
     #[test]
     fn test_part11() {
-        let data: Vec<Fish> = common::read_input::<String>("input/day06.test")
+        let input: Vec<u128> = common::read_input::<String>("input/day06.test")
             .first()
             .unwrap()
             .split(',')
-            .map(|v| v.parse::<Fish>().unwrap())
+            .map(|v| v.parse::<u128>().unwrap())
             .collect();
 
-        let data = simulate(data, 18);
-        let cnt = data.iter().fold(0_u64, |cnt, f| cnt + (f.count as u64));
+        let data = simulate(&input, 18);
+        let cnt: u128 = data.iter().sum();
         assert_eq!(cnt, 26);
     }
 
     #[test]
     fn test_part1() {
-        let data: Vec<Fish> = common::read_input::<String>("input/day06.test")
+        let input: Vec<u128> = common::read_input::<String>("input/day06.test")
             .first()
             .unwrap()
             .split(',')
-            .map(|v| v.parse::<Fish>().unwrap())
+            .map(|v| v.parse::<u128>().unwrap())
             .collect();
 
-        let data = simulate(data, 80);
-        let cnt = data.iter().fold(0_u64, |cnt, f| cnt + (f.count as u64));
+        let data = simulate(&input, 80);
+        let cnt: u128 = data.iter().sum();
         assert_eq!(cnt, 5934);
     }
 
     #[test]
     fn test_part2() {
-        let data: Vec<Fish> = common::read_input::<String>("input/day06.test")
+        let input: Vec<u128> = common::read_input::<String>("input/day06.test")
             .first()
             .unwrap()
             .split(',')
-            .map(|v| v.parse::<Fish>().unwrap())
+            .map(|v| v.parse::<u128>().unwrap())
             .collect();
 
-        //let data = simulate(data, 80);
-        let data = simulate(data, 256);
-        let cnt = data.iter().fold(0, |cnt, f| cnt + f.count as u64);
+        let data = simulate(&input, 256);
+        let cnt: u128 = data.iter().sum();
         assert_eq!(cnt, 26984457539);
     }
 }
