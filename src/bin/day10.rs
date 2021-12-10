894593bf25e7cd6628b6a4dc91bfd423f5ea8c2e
TheCarlG <me@carlg.tech>
2021-12-10 20:37:27 +0100

    day10: Part02 Initial commit

--- src/bin/day10.rs
+++ src/bin/day10.rs
@@ -5,11 +5,10 @@ use advent2021::common;
 const DAY: &str = "day10";
 
 fn part1(lines: &[String]) -> usize {
-    let mut s_map: HashMap<char, i32> = HashMap::new();
-    s_map.insert(')', 3);
-    s_map.insert(']', 57);
-    s_map.insert('}', 1197);
-    s_map.insert('>', 25137);
+    let s_map: HashMap<char, i32> = [(')', 3), (']', 57), ('}', 1197), ('>', 25137)]
+        .iter()
+        .cloned()
+        .collect();
 
     lines
         .iter()
@@ -40,8 +39,55 @@ fn part1(lines: &[String]) -> usize {
         .fold(0, |acc, c| acc + s_map[&c] as usize)
 }
 
-fn part2(l: &[String]) -> usize {
-    0
+fn part2(lines: &[String]) -> u128 {
+    let s_map: HashMap<char, i32> = [(')', 1), (']', 2), ('}', 3), ('>', 4)]
+        .iter()
+        .cloned()
+        .collect();
+
+    let pair: HashMap<char, char> = [('(', ')'), ('[', ']'), ('{', '}'), ('<', '>')]
+        .iter()
+        .cloned()
+        .collect();
+
+    let mut res: Vec<u128> = lines
+        .iter()
+        .filter_map(|l| {
+            let mut stack = Vec::new();
+            for c in l.chars() {
+                match c {
+                    '(' | '{' | '[' | '<' => {
+                        stack.push(c);
+                        continue;
+                    }
+                    ')' | '}' | ']' | '>' => {
+                        let val = stack.pop().unwrap();
+                        if (val == '<' && c == '>')
+                            || (val == '(' && c == ')')
+                            || (val == '[' && c == ']')
+                            || (val == '{' && c == '}')
+                        {
+                            continue;
+                        }
+                        return None;
+                    }
+                    _ => continue,
+                }
+            }
+
+            Some(
+                stack
+                    .iter()
+                    .rev()
+                    .map(|c| pair[c])
+                    .fold(0_u128, |acc, c| (acc * 5) + s_map[&c] as u128),
+            )
+        })
+        .collect();
+
+    res.sort_unstable();
+
+    res[res.len() / 2]
 }
 
 fn main() {
@@ -64,9 +110,8 @@ mod tests {
     }
 
     #[test]
-    #[ignore]
     fn test_part2() {
         let lines = common::read_input::<String>(DAY, true);
-        assert_eq!(part2(&lines), 5);
+        assert_eq!(part2(&lines), 288957);
     }
 }
