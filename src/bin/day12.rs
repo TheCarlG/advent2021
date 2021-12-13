f42d733b4127177b443d75cb89e2e7a348ce4e5d
TheCarlG <me@carlg.tech>
2021-12-13 09:21:07 +0100

    day12: Part 1 clean up and WIP Part 2

--- src/bin/day12.rs
+++ src/bin/day12.rs
@@ -3,28 +3,45 @@ use core::fmt;
 use std::{
     collections::{HashMap, HashSet},
     fmt::Display,
+    io::Write,
 };
 
 const DAY: &str = "day12";
 
-struct Map {
-    nodes: HashMap<i32, Node>,
+struct Map<'a> {
+    nodes: HashMap<&'a str, Node<'a>>,
 }
 
-impl Map {
+impl<'a> Map<'a> {
     fn new() -> Self {
         Self {
             nodes: HashMap::new(),
         }
     }
 
-    fn find_path_to(
-        &self,
-        start: i32,
-        stop: i32,
-        visited: &mut HashSet<i32>,
-        path: &mut Vec<i32>,
-    ) -> i32 {
+    fn read_lines(&mut self, lines: &'a [String]) {
+        lines.iter().for_each(|l| {
+            let (p, c) = l.split_once('-').unwrap();
+
+            self.nodes.entry(p).or_insert_with(|| {
+                let big = p != p.to_lowercase();
+                Node::new(p.to_string(), big)
+            });
+
+            self.nodes.entry(c).or_insert_with(|| {
+                let big = c != c.to_lowercase();
+                Node::new(c.to_string(), big)
+            });
+
+            let parent = self.nodes.get_mut(&p).unwrap();
+            parent.adjecent.push(c);
+
+            let adj = self.nodes.get_mut(&c).unwrap();
+            adj.adjecent.push(p);
+        });
+    }
+
+    fn find_path_to(&self, start: &'a str, stop: &'a str, visited: &mut HashSet<&'a str>) -> i32 {
         if start == stop {
             return 1;
         }
@@ -38,24 +55,65 @@ impl Map {
 
         for cc in &n.adjecent {
             if !visited.contains(cc) {
-                path.push(*cc);
-                num += self.find_path_to(*cc, stop, visited, path);
+                num += self.find_path_to(*cc, stop, visited);
             }
         }
         visited.remove(&start);
 
         num
     }
+
+    fn find_path_to2(
+        &self,
+        start: &'a str,
+        stop: &'a str,
+        visited: &mut HashMap<&'a str, i32>,
+        path: &mut Vec<&'a str>,
+        ext_search: &mut bool,
+    ) -> i32 {
+        if start == stop {
+            println!("{:?}", path);
+            return 1;
+        }
+        let n = &self.nodes[&start];
+
+        let mut ext_updated = false;
+        if !n.big {
+            let val = visited.entry(start).or_insert(0);
+            *val += 1;
+            if *val == 2 && *ext_search {
+                ext_updated = true;
+                *ext_search = false;
+            }
+        }
+
+        let mut num = 0;
+
+        for adj in &n.adjecent {
+            let visits = *visited.get(adj).unwrap_or(&0);
+            if *adj != "start" && (visits < 1 || visits <= 2 && *ext_search) {
+                path.push(start);
+                num += self.find_path_to2(*adj, stop, visited, path, ext_search);
+            }
+        }
+        visited.remove_entry(&start);
+        path.pop();
+        if ext_updated {
+            *ext_search = true;
+        }
+
+        num
+    }
 }
 
 #[derive(Debug)]
-struct Node {
+struct Node<'a> {
     name: String,
     big: bool,
-    adjecent: Vec<i32>,
+    adjecent: Vec<&'a str>,
 }
 
-impl Node {
+impl<'a> Node<'a> {
     fn new(name: String, big: bool) -> Self {
         Node {
             name,
@@ -65,7 +123,7 @@ impl Node {
     }
 }
 
-impl Display for Node {
+impl<'a> Display for Node<'_> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{}", self.name)
     }
@@ -73,61 +131,31 @@ impl Display for Node {
 
 fn part1(lines: &[String]) -> i32 {
     let mut map = Map::new();
-    let mut n_map: HashMap<&str, i32> = HashMap::new();
 
-    lines.iter().for_each(|l| {
-        let parts = l.split_once('-').unwrap();
-        let p = if n_map.contains_key(&parts.0) {
-            n_map[&parts.0]
-        } else {
-            let idx = n_map.len() as i32;
-            n_map.insert(parts.0, idx);
-            idx
-        };
+    map.read_lines(lines);
 
-        let c = if n_map.contains_key(&parts.1) {
-            n_map[&parts.1]
-        } else {
-            let idx = n_map.len() as i32;
-            n_map.insert(parts.1, idx);
-            idx
-        };
-
-        map.nodes.entry(p).or_insert_with(|| {
-            let big = parts.0 != parts.0.to_lowercase();
-            Node::new(parts.0.to_string(), big)
-        });
-
-        map.nodes.entry(c).or_insert_with(|| {
-            let big = parts.1 != parts.1.to_lowercase();
-            Node::new(parts.1.to_string(), big)
-        });
-
-        let parent = map.nodes.get_mut(&p).unwrap();
-        parent.adjecent.push(c);
-
-        let adj = map.nodes.get_mut(&c).unwrap();
-        adj.adjecent.push(p);
-    });
-
-    let start = n_map["start"];
-    let stop = n_map["end"];
-
-    let s = &map.nodes[&start];
+    let s = &map.nodes["start"];
 
     let mut num = 0;
-    for adj in &s.adjecent {
-        let mut visited: HashSet<i32> = HashSet::new();
-        visited.insert(start);
-        let mut path: Vec<i32> = vec![start];
-        num += map.find_path_to(*adj, stop, &mut visited, &mut path);
-    }
+    for _adj in &s.adjecent {}
+    let mut visited: HashSet<&str> = HashSet::new();
+    visited.insert("start");
+    num += map.find_path_to("start", "end", &mut visited);
 
     num
 }
 
-fn part2(_lines: &[String]) -> i32 {
-    0
+fn part2(lines: &[String]) -> i32 {
+    let mut map = Map::new();
+
+    map.read_lines(lines);
+
+    let mut num = 0;
+    let mut visited: HashMap<&str, i32> = HashMap::new();
+    let mut path: Vec<&str> = Vec::new();
+    num += map.find_path_to2("start", "end", &mut visited, &mut path, &mut true);
+
+    num
 }
 
 fn main() {
@@ -163,6 +191,20 @@ mod tests {
     #[ignore]
     fn test_part2() {
         let lines = common::read_input::<String>(DAY, true);
-        assert_eq!(part2(&lines), 5);
+        assert_eq!(part2(&lines), 36);
+    }
+
+    #[test]
+    #[ignore]
+    fn test_part2_2() {
+        let lines = common::read_input::<String>("day12-2", true);
+        assert_eq!(part2(&lines), 103);
+    }
+
+    #[test]
+    #[ignore]
+    fn test_part2_3() {
+        let lines = common::read_input::<String>("day12-3", true);
+        assert_eq!(part2(&lines), 3509);
     }
 }
